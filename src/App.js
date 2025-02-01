3f9b56eb74dece12f29a1a41c9fe3e8bbc582f41
pokala57 <pokalanani2000@gmail.com>
2025-02-01 20:14:26 +0530

    latest push

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div><div className='flexbox'>
+    <h1>5</h1>
+    <div>
+    <button >left</button>
+    <button>right</button>
+    </div>
+    <h1>click on right</h1>
+    <h1>click on left</h1>
+  </div><div className={styles.container}>
+      <div className={styles.box}>1</div>
+      <div className={styles.box}>2</div>
+      <div className={styles.box}>3</div>
+    </div></div>
+    
+  );
+}
+
+export default App;
